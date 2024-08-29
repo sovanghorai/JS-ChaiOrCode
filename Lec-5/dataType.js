2cde5a67d60307747f7f1ffc0c822fa356368a3e
Sovan Ghorai <111166739+sovanghorai@users.noreply.github.com>
2024-08-29 11:10:29 +0000

    Data Type

---
+++ Lec-5/dataType.js
@@ -0,0 +1,24 @@
+"use strict"; //Treat all JS code as Newer Version
+
+
+// Type of Data type
+
+let name = "Sovan" // String Data type
+let number =  123 //Number --->Range 2^53
+let isLoggedIn = true //boolean
+//null => Standaloan value -->Empty
+//Undefine =>iska value abhi tek assigen nahi hua hai
+// Symbol =>Unique ness ko identify ker na ka liya
+//Object => real world entity
+
+
+//Typeof(variable_name) --> Variable ka type jan ka liya use hota hai
+// console.log(typeof "sovan" );
+// console.log(typeof (number));
+
+console.log(typeof (null));  //Return object This the language mistake 
+console.log(typeof (undefined));  //Return Undefine 
+
+
+
+
