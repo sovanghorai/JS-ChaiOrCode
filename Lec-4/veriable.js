7eb62bae7637316db6c3d3e0e5b2c2eb902f0735
Sovan Ghorai <111166739+sovanghorai@users.noreply.github.com>
2024-08-29 11:09:54 +0000

    Variable

---
+++ Lec-4/veriable.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let acountEmail = "sovanghorai6@gmail.com"
+var accountPassword = "123456" // Scope problem that why you can't use 
+accountCity = "Jaipur"
+let acountState ;
+
+//accountId = 2   --> not change
+acountEmail = "Sovanghorai"
+accountPassword = "22222222"
+accountCity = "Bengalury"
+
+console.table([accountId, acountEmail, accountPassword, accountCity, acountState])
+// console.log(acountEmail);
+// console.log(accountPassword);
+// console.log(accountId);
+
+
