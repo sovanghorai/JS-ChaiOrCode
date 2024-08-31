d540263bceb47fd354c06bdef0d2669457b63d5c
Sovan Ghorai <111166739+sovanghorai@users.noreply.github.com>
2024-08-31 14:04:18 +0000

    Comparion Operator

---
+++ Lec-8/Comparition_operator.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 != 1);
+
+// IS tara ki compariotion khbi bhi confution main del sakta hai
+// Confution create
+// console.log("2" == 2); //convert "2" string as a number then compare 
+// console.log("2" > 1);
+
+// Null Comparion 
+// console.log(null > 0);   //NUll conver into 0 then compare 
+// console.log(null >= 0);
+// console.log(null == 0); JS main iska alag rull chal ta hai 
+
+// === Strict check --> matlab a ap ki value ka check ker ta hai sath e sath data type ko bhi check ker ta hai
+
+//console.log("2" === 2);  // false
+
+
+
